a4a2dd23f2e8d7c7006c966ff09cea74d52c160a
emon2021 <mdemonahmed2021@gmail.com>
2023-11-27 16:09:50 +0600

    customization or mastering forgot password template

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,46 @@
 @extends('layouts.app')
-
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
+    <div class="hold-transition login-page">
+        <div class="login-box">
+            <div class="card card-outline card-primary">
+              <div class="card-header text-center">
+                <a href="../../index2.html" class="h1"><b>INFINITY.</b>IT</a>
+              </div>
+              <div class="card-body">
+                @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
+                <p class="login-box-msg">You forgot your password? Here you can easily retrieve a new password.</p>
+                <form action="{{ route('password.email') }}" method="post">
+                    @csrf
+                  <div class="input-group mb-3">
+                    <input type="email" placeholder="E-mail" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    <div class="input-group-append">
+                      <div class="input-group-text">
+                        <span class="fas fa-envelope"></span>
+                      </div>
+                    </div>
+                    @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-12">
+                      <button type="submit" class="btn btn-primary btn-block">Request new password</button>
+                    </div>
+                    <!-- /.col -->
+                  </div>
+                </form>
+                <p class="mt-3 mb-1">
+                  <a href="{{route('login')}}">Login</a>
+                </p>
+              </div>
+              <!-- /.login-card-body -->
             </div>
-        </div>
+          </div>
     </div>
-</div>
 @endsection
